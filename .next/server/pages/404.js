9ae595121bda06da3aa8d4c55b0f58a62f1486b6
Jeffrey0314 <jeffgph0314@gmail.com>
2022-11-08 08:27:11 +0800

    1st commit

---
+++ .next/server/pages/404.js
@@ -0,0 +1,46 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/404";
+exports.ids = ["pages/404"];
+exports.modules = {
+
+/***/ "./pages/404.js":
+/*!**********************!*\
+  !*** ./pages/404.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ PageNotFound)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n\nfunction PageNotFound() {\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"div\", {\n        children: [\n            /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"h1\", {\n                children: \"Page is not found!!\"\n            }, void 0, false, {\n                fileName: \"D:\\\\ProjectFiles\\\\nextjs_tuts\\\\nextjs-course\\\\nextjs-course\\\\nextjs-course-mac-master\\\\pages\\\\404.js\",\n                lineNumber: 4,\n                columnNumber: 13\n            }, this),\n            /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"break\", {}, void 0, false, {\n                fileName: \"D:\\\\ProjectFiles\\\\nextjs_tuts\\\\nextjs-course\\\\nextjs-course\\\\nextjs-course-mac-master\\\\pages\\\\404.js\",\n                lineNumber: 7,\n                columnNumber: 13\n            }, this),\n            /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"h1\", {\n                children: \"Please try again...\"\n            }, void 0, false, {\n                fileName: \"D:\\\\ProjectFiles\\\\nextjs_tuts\\\\nextjs-course\\\\nextjs-course\\\\nextjs-course-mac-master\\\\pages\\\\404.js\",\n                lineNumber: 8,\n                columnNumber: 13\n            }, this)\n        ]\n    }, void 0, true, {\n        fileName: \"D:\\\\ProjectFiles\\\\nextjs_tuts\\\\nextjs-course\\\\nextjs-course\\\\nextjs-course-mac-master\\\\pages\\\\404.js\",\n        lineNumber: 3,\n        columnNumber: 9\n    }, this);\n}\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy80MDQuanMuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7O0FBQUE7QUFBZSxTQUFTQSxlQUFlO0lBQ25DLHFCQUNJLDhEQUFDQzs7MEJBQ0csOERBQUNDOzBCQUFHOzs7Ozs7MEJBR0osOERBQUNDOzs7OzswQkFDRCw4REFBQ0Q7MEJBQUc7Ozs7Ozs7Ozs7OztBQUtoQixDQUFDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vY2xpZW50Ly4vcGFnZXMvNDA0LmpzPzNlZDkiXSwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGRlZmF1bHQgZnVuY3Rpb24gUGFnZU5vdEZvdW5kKCkge1xuICAgIHJldHVybihcbiAgICAgICAgPGRpdj5cbiAgICAgICAgICAgIDxoMT5cbiAgICAgICAgICAgICAgICBQYWdlIGlzIG5vdCBmb3VuZCEhXG4gICAgICAgICAgICA8L2gxPlxuICAgICAgICAgICAgPGJyZWFrIC8+XG4gICAgICAgICAgICA8aDE+XG4gICAgICAgICAgICAgICAgUGxlYXNlIHRyeSBhZ2Fpbi4uLlxuICAgICAgICAgICAgPC9oMT5cbiAgICAgICAgPC9kaXY+XG4gICAgKVxufSJdLCJuYW1lcyI6WyJQYWdlTm90Rm91bmQiLCJkaXYiLCJoMSIsImJyZWFrIl0sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./pages/404.js\n");
+
+/***/ }),
+
+/***/ "react/jsx-dev-runtime":
+/*!****************************************!*\
+  !*** external "react/jsx-dev-runtime" ***!
+  \****************************************/
+/***/ ((module) => {
+
+module.exports = require("react/jsx-dev-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("./pages/404.js"));
+module.exports = __webpack_exports__;
+
+})();
---
+++ pages/404.js
@@ -0,0 +1,13 @@
+export default function PageNotFound() {
+    return(
+        <div>
+            <h1>
+                Page is not found!!
+            </h1>
+            <break />
+            <h1>
+                Please try again...
+            </h1>
+        </div>
+    )
+}
